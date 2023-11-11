59bc12df8103f25a4b93465fb8c1a476a36037d1
franklin3003 <107215914+franklin3003@users.noreply.github.com>
2023-11-11 15:50:43 -0300

    Add files via upload

--- Web design/js/scripts.js
+++ Web design/js/scripts.js
@@ -1,7 +1,21 @@
-const navLinks = document.querySelectorAll('.nav-a')
+// const navLinks = document.querySelectorAll('.nav-a')
 
-navLinks.forEach(item => {
-    item.addEventListener('click', () => {
-        alert('fui clickeado')
-    })
-})
+// navLinks.forEach(item => {
+//     item.addEventListener('click', () => {
+//         alert('fui clickeado')
+//     })
+// })
+
+const user = document.getElementById('user')
+const password = document.getElementById('password')
+const button = document.getElementById('button')
+
+button.addEventListener('click', (e) => {
+    e.preventDefault()
+    const data = {
+        user: user.value,
+        password: password.value 
+    }
+    console.log(data)
+}
+)
